6884dc266aa42e5d15f41731a536b3f8ea6864ad
nouhailazyn <ezzayanynouhaila@gmail.com>
2025-03-13 05:51:20 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+console.error('hello dont use this code its not correct');
+console.log('hello world');
+var certifMicrosof= prompt("Combien de certifcats microsoft Avez-vous");
+var certifOracle= prompt("Combien de certifcats Oracle Avez-vous");
+var certif=parseInt(certifMicrosof)+ parseInt(certifOracle);
+console.log(certif);
+window.print();
+document.write("hello");
+console.log('hello world');
+alert("hello world");
+
+var number1 =prompt("donner le premier nombre ?");
+var number2 =prompt("donner la deuxime nombre ?");
+document.write(prenom);
+
+
+let name = "Nouhaila";
+let age = 20;
+console.log(`hello my name is ${name} and my age is ${age}`)
+console.log('hello my name is' +' '+name+' '+'and my age is'+' '+age)
+
+console.log(Number('5'));
+console.log(+'5' + +'7');
+console.log(parseInt('5 hello'));
+console.log(parseInt('hello 5'));
+console.log(parseInt('5.20'));
+console.log(parseFloat('hello 5'));
+console.log(parseFloat('5.20'));
+console.log(Number.prototype);
+console.log(Number.isSafeInteger(2.220446049250313e-16));
+console.log(Number.isSafeInteger(9007199254740991));
+console.log(Number.isInteger(5));
+console.log(Number.isNaN('nouhaila' + 5))
+console.log(Number.isNaN('nouhaila' - 5))
+console.log(typeof('nouhaila' - 5))
+let zakah = window.prompt();
+console.log('zakah is'+ ' '+ Math.floor(0.025 * zakah));
+let name = 'nouhaila ez-zayany,';
+console.log(name.repeat(2));//répeter le nom
+console.log(name[5]);// index du lettres
+console.log(name.charAt(10));
+console.log(name.indexOf('u'));
+console.log(name.lastIndexOf('e', 10));
+console.log(name.lastIndexOf(10));
